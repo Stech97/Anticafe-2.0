614b660f811ea58bdeff771a2366f4365b4f7ed3
Maxim Stepuchev <maksstech97@gmail.com>
2017-12-01 15:40:49 +0300

    Anticafe-servise
    Изменены информация о сборках

--- Desktop/Anticafe 2.0/Info/Properties/AssemblyInfo.cs
+++ Desktop/Anticafe 2.0/Info/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("Info")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("dll to Anticafe 2.0")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyCompany("Stech97")]
 [assembly: AssemblyProduct("Info")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
