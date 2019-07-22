e3f500b08b029c8f4f3bb84c60af09359db91f15
maksim.stepuchev@quorum.ru <maksim.stepuchev@quorum.ru>
2019-07-22 18:58:32 +0300

    update startwindow

--- Desktop/Windows/StartWindow.xaml.cs
+++ Desktop/Windows/StartWindow.xaml.cs
@@ -7,6 +7,13 @@ namespace Anticafe_4._0
         public StartWindow()
         {
             InitializeComponent();
-        }       
-    }
+        }
+
+		private void BStatrt_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow mainWindow = new MainWindow();
+			mainWindow.Show();
+			Close();
+		}
+	}
 }
