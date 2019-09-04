5b3f7daa1eda039fbc929297d33ecc9f9dc87310
Maxim Stepuchev <maksim.stepuchev@quorum.ru>
2019-09-04 18:33:42 +0300

    add new file

--- Anticafe 4.0 Desktop/Windows/MainWindow.xaml.cs
+++ Anticafe 4.0 Desktop/Windows/MainWindow.xaml.cs
@@ -1,14 +1,68 @@
-﻿using System.Windows;
+﻿using System.Data;
+using System.Windows;
+using System.Data.Entity;
+using Anticafe_4._0_Model;
+using System.Data.SqlClient;
+using Anticafe_4._0_Model.Models;
 
 namespace Anticafe_4._0
 {
     public partial class MainWindow : Window
     {
-        public static string WindowTitle {get; set;}
+        public static string WindowTitle { get; set; }
 
-        public MainWindow()
+		TestContext db = new TestContext();
+
+		public MainWindow()
         {
             InitializeComponent();
-        }
-    }
+
+			WindowTitle = "На смене:";
+
+			/*GuestTable.DataContext = db.GuestInfoes.Local.ToBindingList();
+			Прписать ModelView для изменнеия ячейки и следующей привязкии её в бд
+			Так же изменить строку в бд на свои данные*/
+			SqlConnection MyConnection = new SqlConnection();
+			if (MyConnection.State != ConnectionState.Open)
+			{
+				var cs = MyConnection.State;
+				Logger.Log(cs.ToString());
+				db.GuestInfoes.Load();
+				GuestTable.ItemsSource = db.GuestInfoes.Local.ToBindingList();
+				Logger.Log("Connect to database correct");
+			}
+			else
+			{
+				BNewGuest.IsEnabled = false;
+				Logger.Log("Connect to database isn't open");
+			}
+
+		}
+
+		private void BNewGuest_Click(object sender, RoutedEventArgs e)
+		{
+			NewGuest newGuest = new NewGuest();
+			newGuest.ShowDialog();
+			db.GuestInfoes.Load();
+			GuestTable.ItemsSource = db.GuestInfoes.Local.ToBindingList();
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			e.Cancel = true;
+			Visibility = Visibility.Hidden;
+		}
+
+		private void NotificayExit_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown(1);
+			Logger.Log("Close Application with notification icon");
+			Logger.Log("Exit");
+		}
+
+		private void NotificayVisibly_Click(object sender, RoutedEventArgs e)
+		{
+			Visibility = Visibility.Visible;
+		}
+	}
 }
--- Anticafe 4.0 Desktop/Windows/NewGuest.xaml.cs
+++ Anticafe 4.0 Desktop/Windows/NewGuest.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using Anticafe_4._0_Model.Models;
 
 namespace Anticafe_4._0
 {
@@ -8,5 +10,25 @@ namespace Anticafe_4._0
         {
             InitializeComponent();
         }
-    }
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			TestContext db = new TestContext();
+
+			GuestInfo gi = new GuestInfo
+			{
+				FirstName = BName.Text,
+				SecondName = BSName.Text,
+				MiddleName = BMName.Text,
+				Discount = int.Parse(BDis.Text),
+				BDay = DateTime.Parse(BBD.Text),
+				Email = BEmail.Text,
+				Phone = BPhone.Text
+			};
+
+			db.GuestInfoes.Add(gi);
+			db.SaveChanges();
+			Close();
+		}
+	}
 }
