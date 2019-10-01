c77f6f968ae7b098bceddf8f0702a42e7ce849fe
Maxim Stepuchev <maksstech97@gmail.com>
2019-10-01 23:51:49 +0300

    update link icon

--- Anticafe 4.0 Model/Logger.cs
+++ Anticafe 4.0 Model/Logger.cs
@@ -12,33 +12,69 @@ namespace Anticafe_4._0_Model
         public static void TraceLog(string trace)
         {
             var _path = "C:\\log\\Anticafe";
-
-            _path = _path + "\\trace_" + DateTime.Now.Date.ToShortDateString() + ".log";
-			using (StreamWriter logger = new StreamWriter(_path, true))
-			{
-                if (trace.Equals("Exit"))
+            
+            if (Directory.Exists(_path))
+            {
+                _path = _path + "\\trace_" + DateTime.Now.Date.ToShortDateString() + ".log";
+                using (StreamWriter logger = new StreamWriter(_path, true))
                 {
-                    logger.WriteLine();
-                    logger.WriteLine();
-                    logger.WriteLine();
-                    logger.WriteLine();
+                    if (trace.Equals("Exit"))
+                    {
+                        logger.WriteLine();
+                        logger.WriteLine();
+                        logger.WriteLine();
+                        logger.WriteLine();
+                    }
+                    else
+                        logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + trace);
                 }
-                else
-                    logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + trace);
-			}
+            }
+            else
+            {
+                Directory.CreateDirectory(_path);
+
+                _path = _path + "\\trace_" + DateTime.Now.Date.ToShortDateString() + ".log";
+                using (StreamWriter logger = new StreamWriter(_path, true))
+                {
+                    if (trace.Equals("Exit"))
+                    {
+                        logger.WriteLine();
+                        logger.WriteLine();
+                        logger.WriteLine();
+                        logger.WriteLine();
+                    }
+                    else
+                        logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + trace);
+                }
+            }
 		}
 
 		public static void ExeptionLog(string exp)
 		{
             var _path = "C:\\log\\Anticafe";
 
-            _path = _path + "\\exeption_" + DateTime.Now.Date.ToShortDateString() + ".log";
-			using (StreamWriter logger = new StreamWriter(_path, true))
-			{
-                logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + exp);
-            }
+            if (Directory.Exists(_path))
+            {
+                _path = _path + "\\exeption_" + DateTime.Now.Date.ToShortDateString() + ".log";
+                using (StreamWriter logger = new StreamWriter(_path, true))
+                {
+                    logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + exp);
+                }
 
-            SaveToDB.SaveExeptionToDB(DateTime.Now, exp);
+                SaveToDB.SaveExeptionToDB(DateTime.Now, exp);
+            }
+            else
+            {
+                Directory.CreateDirectory(_path);
+
+                _path = _path + "\\exeption_" + DateTime.Now.Date.ToShortDateString() + ".log";
+                using (StreamWriter logger = new StreamWriter(_path, true))
+                {
+                    logger.WriteLine(DateTime.Now.ToLongTimeString() + " - " + exp);
+                }
+
+                SaveToDB.SaveExeptionToDB(DateTime.Now, exp);
+            }
 		}
 
 	}
